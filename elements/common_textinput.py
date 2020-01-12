d389bb2fb2b3bb07827a452d38973ec1c29af98d
snckmykek <1ou3@mail.ru>
2020-01-12 03:27:56 +0300

    4

--- elements/common_textinput.py
+++ elements/common_textinput.py
@@ -1,9 +1,9 @@
-from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.lang import Builder
 
 Builder.load_file(r'elements/common_textinput.kv')
 
 
-class CommonTextInput(Button):
+class CommonTextInput(TextInput):
     def __init__(self, **kwargs):
         super(CommonTextInput, self).__init__(**kwargs)
